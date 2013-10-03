d1a0eb5c2e8b1c9d9637b7ecacff14824c6e6777
Mathieu Martin <webmat@gmail.com>
2013-10-02 23:09:47 -0400

    Create an empty main.yml, while we're at it.

--- lib/ansible-helper/cli.rb
+++ lib/ansible-helper/cli.rb
@@ -10,9 +10,16 @@ class AnsibleHelper::CLI < Thor
           :desc => "Create a .gitkeep file in each subdirectory?",
           :type => :boolean, :default => false, :banner => 'BOOLEAN'
   def role(name)
-    dir_glob = "roles/#{name}/{#{options[:dirs]}}"
+    role_dir = "roles/#{name}"
+    dir_glob = "#{role_dir}/{#{options[:dirs]}}"
+
     `mkdir -p #{dir_glob}`
     `touch #{dir_glob}/.gitkeep` if options[:gitkeep]
-    puts Dir["roles/#{name}/**/*"]
+
+    if Dir["#{role_dir}/tasks"].any?
+      File.write("#{role_dir}/tasks/main.yml", "---\n")
+    end
+
+    puts Dir["#{role_dir}/**/*"]
   end
 end
